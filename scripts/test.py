07fa99bad98b6217789e5c0e8f54063d79d8e441
Sébastien Campion <sebastien.campion@foss4.eu>
2023-09-05 17:35:48 +0200

    update

--- scripts/test.py
+++ scripts/test.py
@@ -31,17 +31,17 @@ for i in range(1, len(indices)):
     indices[i] += indices[i - 1]
 
 
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return left
+    def binary_search(arr, target):
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
 
 
 results = {}
